58032444df43d882c9c34580284df0779737b366
Slava_Vidishuk <80291668219@mail.ru>
2022-12-24 18:44:08 +0300

    Added user restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,18 @@
 class UsersController < ApplicationController
 
+    before_action :set_user, only: [:show, :edit, :update]
+
     def new
         @user = User.new
         
     end
 
     def edit
-        @user = User.find(params[:id])
+       
     end
 
     def update
-        @user = User.find(params[:id])
+        
         if @user.update(user_params)
             flash[:notice] = "Your account info was updated"
             redirect_to @user  
@@ -21,7 +23,7 @@ class UsersController < ApplicationController
     end
 
         def show
-            @user= User.find(params[:id ])
+            
             @articles = @user.articles.paginate(page: params[:page], per_page: 5)
         end
 
@@ -32,6 +34,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to the alpha blog, #{@user.username},your have sucesfully signed up"
             redirect_to articles_path 
         else
@@ -42,6 +45,10 @@ class UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(:username,:email,:password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 
 end
